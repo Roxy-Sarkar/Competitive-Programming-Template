017b5dc50eef15611e245a557ebfd5f68960ebce
Roxy_Sarkar <roxysarkar2030@gmail.com>
2021-04-27 01:57:43 -0700

    File renamed

---
+++ Competitive pogramming template/temp.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define gap " "
+#define endl "\n"
+#define pie acos(-1)
+#define mod 1000000007
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ll test;
+    cin >> test;
+
+    while (test--)
+    {
+    }
+}
+// Thanking you by Roxy Sarkar
